a0bf69e38ecb23d002096c8535f0af38fae51e98
aslam54321 <41164116+MdAslam54321@users.noreply.github.com>
2025-07-20 15:12:18 +0530

    Added printing commnent in Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Test t = new Test();
         String sentence = "Jackie Chan is the best hollywood actor";
+        // Printing original sentence
         System.out.println(sentence);
         String reversedWords = t.reverseEachWord(sentence);
         System.out.println(reversedWords);
