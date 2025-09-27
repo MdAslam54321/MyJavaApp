54997acb328a6c2b06b15b6be171be6b4effb62d
aslam54321 <41164116+MdAslam54321@users.noreply.github.com>
2025-09-27 12:14:24 +0530

    Added BRUCE_LEE Constant in Util

--- src/com/java/MyJavaApp/util/Constant.java
+++ src/com/java/MyJavaApp/util/Constant.java
@@ -2,4 +2,5 @@ package com.java.MyJavaApp.util;
 
 public class Constant {
     public static String JACKIE_CHAN = "Jackie Chan is the best hollywood actor";
+    public static String BRUCE_LEE = "Bruce Lee is on of the best actors";
 }
